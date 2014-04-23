841b4d777df33451e3da0fe29c49ad256fbb07a0
KenZhouGreat <kainzhou@gmail.com>
2014-04-23 17:05:24 +1000

    last commit

---
+++ Dev/static/js/legals.js
@@ -0,0 +1,1 @@
+$(document).ready(function (){if( /iPhone|iPad/i.test(navigator.userAgent)){if(!/Safari/.test(navigator.userAgent)){/*$('#display').text('Loaded inside App');*/$("header").css("display", "none");$("footer").css("display", "none");}else{/* $('#display').text('Loaded inside iPhone/iPad with Safari');*/}}else{/*$('#display').text('Not loaded from iPhone/iPad');*/}});
